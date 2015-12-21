f6353c7bdc217d957bdc6298264ffa27b3afd015
Marko Papic <marko.papic@devtechgroup.com>
2015-12-21 10:59:18 +0100

    Added test project

---
+++ VersionTesting/VersionTesting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionTesting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+			string version = fileVersionInfo.ProductVersion;
+			Console.WriteLine("Informational version: {0}", version);
+
+			Console.ReadKey();
+		}
+	}
+}
