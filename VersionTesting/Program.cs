589f16392b67b7f2493b2b26074d5bd9f1851ea8
Marko Papic <marko.papic@devtechgroup.com>
2015-12-21 12:12:09 +0100

    Extracted project to repository root

--- VersionTesting/Program.cs
+++ VersionTesting/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VersionTesting
 {
@@ -11,8 +15,6 @@ namespace VersionTesting
 			Assembly assembly = Assembly.GetExecutingAssembly();
 			FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
 			string version = fileVersionInfo.ProductVersion;
-			Console.WriteLine("Hello Dear");
-			Console.WriteLine("Hello Dear");
 			Console.WriteLine("Informational version: {0}", version);
 
 			Console.ReadKey();
