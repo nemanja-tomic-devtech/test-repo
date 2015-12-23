062e3d6892f5c6ca4c594695eaeff3bb81bb41e7
Nemanja Tomic <nemanja.tomic@devtechgroup.com>
2015-12-23 10:22:32 +0100

    Updated version batch script.

--- test.bat
+++ test.bat
@@ -2,6 +2,25 @@
 >output.tmp git describe --abbrev=0 --tags
 
 <output.tmp (
-  set /p line1=
+  set /p currenttag=
 )
-echo %line1%
+
+>branch.tmp git rev-parse --abbrev-ref HEAD
+<branch.tmp (
+	set /p storedbranch=
+)
+
+if %storedbranch%==master (
+	set finalversion=%currenttag%
+) else if %storedbranch%==develop (
+	set finalversion=%currenttag%-dev
+) else if %storedbranch:~0,7%==feature (
+	set finalversion=%currenttag%-dev
+) else if %storedbranch:~0,7%==release (
+	set finalversion=%storedbranch:~8%-rc
+) else if %storedbranch:~0,6%==hotfix (
+	set finalversion=%storedbranch:~7%-rc
+) else if %storedbranch:~0,7%==support (
+	set finalversion=%currenttag%-%storedbranch:~8%
+)
+echo %finalversion%
